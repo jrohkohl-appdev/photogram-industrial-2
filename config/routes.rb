7e45868e6a33c7cd9303a14753cbd2cbe7ef03b0
jrohkohl <jrohkohl@chicagobooth.edu>
2021-04-22 05:39:16 +0000

    set

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get "/users:id" => "user#show", as: :user
+  resources :users, only: [:show]
+
+  
+
+
 end
