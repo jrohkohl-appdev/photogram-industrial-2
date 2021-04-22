f5313e13e35a6db21928571976608b2cddfcee01
jrohkohl <jrohkohl@chicagobooth.edu>
2021-04-22 20:16:38 +0000

    added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end 
 
 
 end
